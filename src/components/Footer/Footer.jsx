eb5f9a7e38eeaa7efab064d52fd243f50e268981
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-10-28 15:45:29 +0100

    Footer Creation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
-
+import './Footer.css'
 
 function Footer() {
     return (
         <footer>
-            footer
+            <span className="copyright">© 2024 Sam Gleize</span>
         </footer>
     )
 }
