07d92335d4fcb454ed4fe22e6913b7eea47db431
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-10-28 15:28:46 +0100

    Project component creation

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,14 @@
+import './Project.css'
+
+function Project() {
+
+    return (
+        <section className="project-title">
+            <h2>MES REALISATIONS</h2>
+            <p>Voici quelques-uns des projets sélectionnés qui témoignent de ma passion pour le développement.</p>
+        </section>
+    )
+
+}
+
+export default Project
