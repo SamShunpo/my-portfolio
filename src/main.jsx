bd8f34362bc4f3b6bb5bda2471618252ebbed929
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-11-04 14:19:56 +0100

    delete gh_pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,10 @@ import ErrorPage from './pages/ErrorPage/ErrorPage'
 import '../src/styles/index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const basename = import.meta.env.MODE === 'production' ? "my-portfolio" : ""; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
