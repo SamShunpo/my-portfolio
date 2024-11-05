411aa9f92e71f2c89cb8f51112ef76c980a0e446
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-11-05 15:50:32 +0100

    add project page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
+import ProjectPage from './pages/ProjectPage/ProjectPage'
 import '../src/styles/index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path="/project/:id" element={<ProjectPage/>}/>
         <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </BrowserRouter>
