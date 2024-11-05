56caff266c6a41699364447f0c127c47a393adce
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-11-05 15:51:56 +0100

    project pages creation

---
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,15 @@
+import './ProjectPage.css'
+import Layout from '../../components/Layout/Layout'
+
+function ProjectPage() {
+
+  return (
+    <Layout>
+      <main>
+        <h1>Project page</h1>
+      </main>
+    </Layout>
+  )
+}
+
+export default ProjectPage
