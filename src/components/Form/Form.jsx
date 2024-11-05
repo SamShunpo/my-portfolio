eedb11661ec8f5008e9b2e39c73cc0001390cae8
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-11-05 15:50:46 +0100

    update form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,25 +2,37 @@ import './Form.css'
 
 function Form() {
     return (
-        <section id="form" className='form'>
-            <div>
+        <section id="form" className='form-container'>
+            <div className="form-left">
                 <h2>Contact</h2>
-                <a href="mailto:gleizebourrassamuel@gmail.com">gleizebourrassamuel@gmail.com</a>
+                <span className="span-form">Un petit bonjour : <a href="mailto:gleizebourrassamuel@gmail.com" className="a-form">gleizebourrassamuel@gmail.com</a></span>
+                <span className="span-form"> Pour plus d'info : <a href="/images/cv_sam.pdf" download className="a-form">mon CV</a></span>
+                <div className="rs-form-container">
+                    <a href="https://www.linkedin.com/in/samuel-gleize-bourras-a8458a100/" className='rs' target="_blank"><img src="images/bxl-linkedin.svg.svg" alt="Icon Linkedin" /></a>
+                    <a href="https://github.com/SamShunpo" className='rs'target="_blank"><img src="images/bxl-github.svg" alt="Icon Github" /></a>
+                </div>
             </div>
             <form name="contact" method="POST" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact" />
-                <p>
-                    <label>Nom : <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Email : <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <label>Message : <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Envoyer</button>
-                </p>
+                <div>
+                    <label htmlFor='name'>Nom</label>
+                    <input type="text" name="name" id='name' />
+                </div>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" name="email" id='email' />
+                </div>
+                <div>
+                    <label htmlFor='subject'>Sujet</label>
+                    <input type="text" name="subject" id='subject' />
+                </div>
+                <div>
+                    <label htmlFor='message'>Message</label>
+                    <textarea name="message" id='message'></textarea>
+                </div>
+                <div>
+                    <button type="submit" className='button-submit'>ENVOYER</button>
+                </div>
             </form>
 
         </section>
