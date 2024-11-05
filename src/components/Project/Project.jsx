aa89c31cde6c9e73fde04e69be1ee7b3bac5205b
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-11-05 15:51:14 +0100

    use dynamique data

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,12 +1,19 @@
 import './Project.css'
 
-function Project() {
+function Project({ cover, title, description, tag }) {
 
     return (
-        <section className="project-title">
-            <h2>MES REALISATIONS</h2>
-            <p>Voici quelques-uns des projets sélectionnés qui témoignent de ma passion pour le développement.</p>
-        </section>
+            <article>
+                <div className='article-left'>
+                    <span className='tag'>{tag}</span>
+                    <img src={cover} alt="" className='article-image' />
+                </div>
+                <div className='article-right'>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <h4>INFO DU PROJET</h4>
+                </div>
+            </article>
     )
 
 }
