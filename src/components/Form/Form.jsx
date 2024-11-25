40538b130676d800c71fcb89056bbe5d1c71a171
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-11-25 22:26:34 +0100

    Add security to form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,63 @@
-import './Form.css'
+import './Form.css';
+import React, { useState } from 'react';
+
 
 function Form() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+
+        setErrors({
+            ...errors,
+            [name]: '',
+        });
+    };
+
+    const validate = () => {
+        const newErrors = {};
+
+        if (!formData.name.trim()) {
+            newErrors.name = 'Le champ Nom est obligatoire.';
+        }
+
+        if (!formData.email.trim()) {
+            newErrors.email = 'Le champ Email est obligatoire.';
+          } 
+
+        if (!formData.subject.trim()) {
+            newErrors.subject = 'Le champ Sujet est obligatoire.';
+        }
+
+        if (!formData.message.trim()) {
+            newErrors.message = 'Le champ Message est obligatoire.';
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0; 
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (validate()) {
+            console.log('Formulaire soumis avec succès :', formData);
+        } else {
+            console.log('Formulaire invalide.');
+        }
+    };
+
     return (
         <section id="form">
             <div className="form-left">
@@ -8,34 +65,74 @@ function Form() {
                 <span className="span-form">Un petit bonjour : <a href="mailto:gleizebourrassamuel@gmail.com" className="a-form">gleizebourrassamuel@gmail.com</a></span>
                 <span className="span-form"> Pour plus d'info : <a href="/images/cv_sam.pdf" download className="a-form">mon CV</a></span>
                 <div className="rs-form-container">
-                    <a href="https://www.linkedin.com/in/samuel-gleize-bourras-a8458a100/" className='rs' target="_blank"><img src="images/bxl-linkedin.svg.svg" alt="Icon Linkedin" /></a>
-                    <a href="https://github.com/SamShunpo" className='rs'target="_blank"><img src="images/bxl-github.svg" alt="Icon Github" /></a>
+                    <a href="https://www.linkedin.com/in/samuel-gleize-bourras-a8458a100/" className='rs' target="_blank"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className='svg-rs'>
+                        <rect width="32" height="32" fill="black" />
+                        <path d="M6.644 9.59602C8.2552 9.59602 9.56134 8.28988 9.56134 6.67869C9.56134 5.06749 8.2552 3.76135 6.644 3.76135C5.0328 3.76135 3.72667 5.06749 3.72667 6.67869C3.72667 8.28988 5.0328 9.59602 6.644 9.59602Z" fill="#D3E97A" />
+                        <path d="M12.316 11.8067V27.992H17.3413V19.988C17.3413 17.876 17.7387 15.8307 20.3573 15.8307C22.94 15.8307 22.972 18.2453 22.972 20.1213V27.9933H28V19.1173C28 14.7573 27.0613 11.4067 21.9653 11.4067C19.5187 11.4067 17.8787 12.7493 17.208 14.02H17.14V11.8067H12.316ZM4.12666 11.8067H9.16V27.992H4.12666V11.8067Z" fill="#D3E97A" />
+                    </svg></a>
+                    <a href="https://github.com/SamShunpo" className='rs' target="_blank"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className='svg-rs'>
+                        <rect width="32" height="32" fill="black" />
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M16.0347 2.66669C8.68933 2.66669 2.73599 8.62002 2.73599 15.9654C2.73599 21.84 6.54533 26.8254 11.8307 28.5854C12.496 28.7054 12.736 28.296 12.736 27.944C12.736 27.628 12.7253 26.7907 12.7213 25.6827C9.02133 26.4854 8.23999 23.8987 8.23999 23.8987C7.63733 22.3627 6.76399 21.9534 6.76399 21.9534C5.55733 21.128 6.85599 21.1467 6.85599 21.1467C8.19199 21.24 8.89199 22.5174 8.89199 22.5174C10.0787 24.5494 12.0067 23.9627 12.7613 23.6227C12.8827 22.7627 13.2293 22.176 13.608 21.844C10.656 21.5094 7.55199 20.368 7.55199 15.2707C7.55199 13.8214 8.07066 12.632 8.91733 11.704C8.78266 11.3667 8.32266 10.0134 9.04933 8.18402C9.04933 8.18402 10.1653 7.82535 12.7053 9.54535C13.7901 9.25025 14.9091 9.09961 16.0333 9.09735C17.1576 9.09918 18.2766 9.24982 19.3613 9.54535C21.9027 7.82402 23.0173 8.18402 23.0173 8.18402C23.744 10.0134 23.288 11.3667 23.1493 11.704C24.0027 12.632 24.5147 13.82 24.5147 15.2707C24.5147 20.3814 21.408 21.504 18.4453 21.8334C18.9187 22.244 19.3453 23.0547 19.3453 24.2947C19.3453 26.0734 19.3293 27.508 19.3293 27.944C19.3293 28.3 19.5667 28.7134 20.2453 28.5827C25.528 26.82 29.3333 21.8387 29.3333 15.9654C29.3333 8.62002 23.38 2.66669 16.0347 2.66669Z" fill="#D3E97A" />
+                    </svg></a>
                 </div>
             </div>
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
                 <input type="hidden" name="form-name" value="contact" />
+
                 <div>
-                    <label htmlFor='name'>Nom</label>
-                    <input type="text" name="name" id='name' />
+                    <label htmlFor="name">Nom</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                    {errors.name && <span className="error">{errors.name}</span>}
                 </div>
+
                 <div>
-                    <label htmlFor='email'>Email</label>
-                    <input type="email" name="email" id='email' />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    {errors.email && <span className="error">{errors.email}</span>}
                 </div>
+
                 <div>
-                    <label htmlFor='subject'>Sujet</label>
-                    <input type="text" name="subject" id='subject' />
+                    <label htmlFor="subject">Sujet</label>
+                    <input
+                        type="text"
+                        name="subject"
+                        id="subject"
+                        value={formData.subject}
+                        onChange={handleChange}
+                    />
+                    {errors.subject && <span className="error">{errors.subject}</span>}
                 </div>
+
                 <div>
-                    <label htmlFor='message'>Message</label>
-                    <textarea name="message" id='message'></textarea>
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                    ></textarea>
+                    {errors.message && <span className="error">{errors.message}</span>}
                 </div>
+
                 <div>
-                    <button type="submit" className='button-submit'>ENVOYER</button>
+                    <button type="submit" className="button-submit">
+                        ENVOYER
+                    </button>
                 </div>
             </form>
-
         </section>
-    )
+    );
 }
-export default Form
+ export default Form;
