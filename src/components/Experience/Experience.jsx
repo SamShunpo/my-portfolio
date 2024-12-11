f3c561d35cc7204c9b64f7cea9477958b701fcd4
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-12-11 22:05:05 +0100

    update experiece style

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,10 +1,12 @@
+import './Experience.css'
+
 function Experience({ title, date, id, company, company_link, description }) {
 
     return (
-        <article key={id}>
-            <div>
-            <h4>{title}</h4>
-            <span>{date}</span>
+        <article key={id} className="article-exp">
+            <div className='header-exp'>
+            <h5>{title}</h5>
+            <p >{date}</p>
             </div>
             <a href={company_link}>{company}</a>
             <p>{description}</p>
