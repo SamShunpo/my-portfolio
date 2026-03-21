46be2e572dac1d04a376c09536437b5dd28f0631
SamShunpo <143656335+SamShunpo@users.noreply.github.com>
2026-03-21 22:22:15 +0100

    Update copyright year in Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer() {
     return (
         <footer>
-            <span className="copyright">© 2024 Sam Gleize</span>
+            <span className="copyright">© 2026 Sam Gleize</span>
         </footer>
     )
 }
